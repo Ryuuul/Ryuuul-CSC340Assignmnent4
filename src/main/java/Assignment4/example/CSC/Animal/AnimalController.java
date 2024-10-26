066d70f3ae4d6fe704d33473ed32345f4bf758e6
Ryuuul <ryadav57804@gmail.com>
2024-10-25 21:18:57 -0400

    MVC

--- src/main/java/Assignment4/example/CSC/Animal/AnimalController.java
+++ src/main/java/Assignment4/example/CSC/Animal/AnimalController.java
@@ -2,14 +2,21 @@ package Assignment4.example.CSC.Animal;
 
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
-import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 //@RestController
-@RestController
+@Controller
 @RequestMapping("/animals")
 public class AnimalController {
 
@@ -19,49 +26,76 @@ public class AnimalController {
 
     //http://localhost:8080/animals/all
     @GetMapping("/all")
-    public List<Animal> getAllAnimals() {
-        return service.getAllAnimals();
+    public String getAllAnimals(Model model) {
+        List<Animal> animals = service.getAllAnimals();
+        model.addAttribute("animalList", animals); 
+        return "animal-list"; 
     }
+
 
     //http://localhost:8080/animals/{id}
     @GetMapping("/{animalId}")
-    public Animal getOneAnimal(@PathVariable int animalId) {
-        return service.getAnimalById(animalId);
+    public String getOneAnimal(@PathVariable int animalId, Model model) {
+        Animal animal = service.getAnimalById(animalId);
+        model.addAttribute("animal", animal); 
+        return "animal-details"; 
     }
-    
+
+    //show the creation form
     //http://localhost:8080/animals/new
+    @GetMapping("/new")
+    public String showCreateForm(Model model) {
+        model.addAttribute("animal", new Animal()); 
+        return "animal-create"; 
+    }
+
     @PostMapping("/new")
-    public List<Animal> addAnimal(@RequestBody Animal animal) {
+    public String addAnimal(Animal animal) {
         service.addAnimal(animal);
-        return service.getAllAnimals();
+        return "redirect:/animals/all"; 
     }
 
 
     //http://localhost:8080/animals/name_search?name=
     @GetMapping("/name_search")
-    public List<Animal> searchByName(@RequestParam(name = "name", defaultValue = "") String keyword) {
-        return service.searchByName(keyword);
+    public String searchByName(@RequestParam(name = "name", defaultValue = "") String keyword, Model model) {
+        List<Animal> animals = service.searchByName(keyword);
+        model.addAttribute("animalList", animals); 
+        return "animal-list"; 
     }
+
 
     //http://localhost:8080/animals/search_species?species=
     @GetMapping("/search_species")
-    public List<Animal> searchBySpecies(@RequestParam(name = "species", defaultValue = "") String species) {
-        return service.searchBySpecies(species);
+    public String searchBySpecies(@RequestParam(name = "species", defaultValue = "") String species, Model model) {
+        List<Animal> animals = service.searchBySpecies(species);
+        model.addAttribute("animalList", animals); 
+        return "animal-list"; 
     }
-    
-    //http://localhost:8080/animals/update/{id}
-    @PutMapping("/update/{animalId}")
-    public Animal updateAnimal(@PathVariable int animalId, @RequestBody Animal animal) {
-        service.updateAnimal(animalId, animal);
 
-        return service.getAnimalById(animalId);
+
+    //http://localhost:8080/animals/update/{id}
+    //show the update form
+    @GetMapping("/update/{animalId}")
+    public String showUpdateForm(@PathVariable int animalId, Model model) {
+        model.addAttribute("animal", service.getAnimalById(animalId)); 
+        return "animal-update"; 
+    }
+
+    
+    //update the animal
+    
+    @PostMapping("/update")
+    public String updateAnimal(Animal animal) {
+        service.updateAnimal(animal.getAnimalId(), animal);
+        return "redirect:/animals/" + animal.getAnimalId();
     }
     
     //http://localhost:8080/animals/delete/{id}
-    @DeleteMapping("/delete/{animalId}")
-    public List<Animal> deleteAnimalById(@PathVariable int animalId) {
+    @GetMapping("/delete/{animalId}")
+    public String deleteAnimalById(@PathVariable int animalId) {
         service.deleteAnimal(animalId);
-        return service.getAllAnimals();
+        return "redirect:/animals/all";
     }
 
 
